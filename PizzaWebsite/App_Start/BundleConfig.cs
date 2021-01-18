2ce5ebff96edbf1a6f36546a83a4b5ef34bbf1a3
Michael Rees <mrees791@gmail.com>
2021-01-18 13:52:15 -0500

    Implemented Font Awesome.

--- PizzaWebsite/App_Start/BundleConfig.cs
+++ PizzaWebsite/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace PizzaWebsite
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/fontawesome.css",
                       "~/Content/site.css"));
         }
     }
