c4741bf03db8673134a514f9890c9bb0db9e7e8d
Michael Rees <mrees791@gmail.com>
2021-01-23 20:04:19 -0500

    Added user table.

---
+++ PizzaDatabase/dbo/tables/user.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[user]
+(
+  [user_id] INT NOT NULL IDENTITY,
+  [current_cart_id] INT NOT NULL,
+  [email] VARCHAR(256) NOT NULL,
+  [password_hash] NVARCHAR(MAX) NOT NULL,
+  [is_banned] BIT NOT NULL,
+  [email_confirmed] BIT NOT NULL,
+  [phone_number] VARCHAR(10) NOT NULL,
+  [phone_number_confirmed] BIT NOT NULL,
+  [zip_code] VARCHAR(5) NOT NULL,
+  PRIMARY KEY ([user_id])
+);
