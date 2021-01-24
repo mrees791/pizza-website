ee0f200cb2d1ab74ab9493ae2a613ac2e9b3fc3b
Michael Rees <mrees791@gmail.com>
2021-01-24 17:39:09 -0500

    Updated non-null fields in user table.

--- PizzaDatabase/dbo/tables/user.sql
+++ PizzaDatabase/dbo/tables/user.sql
@@ -6,8 +6,8 @@
   [password_hash] NVARCHAR(MAX) NOT NULL,
   [is_banned] BIT NOT NULL,
   [email_confirmed] BIT NOT NULL,
-  [phone_number] VARCHAR(10) NOT NULL,
+  [phone_number] VARCHAR(10),
   [phone_number_confirmed] BIT NOT NULL,
-  [zip_code] VARCHAR(5) NOT NULL,
+  [zip_code] VARCHAR(5),
   PRIMARY KEY ([user_id])
 );
