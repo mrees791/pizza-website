044ae0b82d1856d86b97f47959d432e6e57244a3
Michael Rees <mrees791@gmail.com>
2021-01-24 19:46:25 -0500

    Added fields to cart_sauce table.

--- PizzaDatabase/dbo/tables/cart_sauce.sql
+++ PizzaDatabase/dbo/tables/cart_sauce.sql
@@ -1,4 +1,11 @@
 ﻿CREATE TABLE [dbo].[cart_sauce]
 (
-	[Id] INT NOT NULL PRIMARY KEY
-)
+  [cart_sauce_id] INT NOT NULL IDENTITY,
+  [cart_id] INT NOT NULL,
+  [menu_sauce_id] INT NOT NULL,
+  [price_per_item] DECIMAL(20,2) NOT NULL,
+  [quantity] INT NOT NULL,
+  PRIMARY KEY ([cart_sauce_id]),
+  FOREIGN KEY ([cart_id]) REFERENCES cart(cart_id),
+  FOREIGN KEY ([menu_sauce_id]) REFERENCES menu_sauce(menu_sauce_id)
+);
--- PizzaDatabase/dbo/tables/menu_sauce.sql
+++ PizzaDatabase/dbo/tables/menu_sauce.sql
@@ -1,4 +1,4 @@
 ﻿CREATE TABLE [dbo].[menu_sauce]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[menu_sauce_id] INT NOT NULL PRIMARY KEY
 )
