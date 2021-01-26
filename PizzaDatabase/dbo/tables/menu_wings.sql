77708069892aa45329a7b26a98c2a6c84089ab40
Michael Rees <mrees791@gmail.com>
2021-01-26 14:34:08 -0500

    Added fields to menu_wings table.

--- PizzaDatabase/dbo/tables/menu_wings.sql
+++ PizzaDatabase/dbo/tables/menu_wings.sql
@@ -1,4 +1,13 @@
 ﻿CREATE TABLE [dbo].[menu_wings]
 (
-	[menu_wings_id] INT NOT NULL PRIMARY KEY
-)
+  [menu_wings_id] INT NOT NULL IDENTITY,
+  [available_for_purchase] BIT NOT NULL,
+  [name] VARCHAR(100) NOT NULL,
+  [price_6_piece] DECIMAL(20,2) NOT NULL,
+  [price_12_piece] DECIMAL(20,2) NOT NULL,
+  [price_18_piece] DECIMAL(20,2) NOT NULL,
+  [description] VARCHAR(512) NOT NULL,
+  [has_menu_icon] BIT NOT NULL,
+  [menu_icon_file] VARCHAR(50) NOT NULL,
+  PRIMARY KEY ([menu_wings_id])
+);
