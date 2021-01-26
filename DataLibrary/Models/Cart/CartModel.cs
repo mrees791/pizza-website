bdbdde8761a30aa6ff715db74cabd6eb0f0f3889
Michael Rees <mrees791@gmail.com>
2021-01-26 17:17:19 -0500

    Created model for cart_id table.

---
+++ DataLibrary/Models/Cart/CartModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models.Cart
+{
+    public class CartModel
+    {
+        public int CartId { get; set; }
+    }
+}
