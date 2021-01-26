f2f2ee3fb4e3d03f7d3b62834b24c96bff4eccc5
Michael Rees <mrees791@gmail.com>
2021-01-26 14:21:00 -0500

    Added fields to menu_dip table.

--- PizzaDatabase/dbo/tables/menu_dip.sql
+++ PizzaDatabase/dbo/tables/menu_dip.sql
@@ -1,4 +1,11 @@
 ﻿CREATE TABLE [dbo].[menu_dip]
 (
-	[menu_dip_id] INT NOT NULL PRIMARY KEY
-)
+  [menu_dip_id] INT NOT NULL IDENTITY,
+  [available_for_purchase] BIT NOT NULL,
+  [name] VARCHAR(100) NOT NULL,
+  [price] DECIMAL(20,2) NOT NULL,
+  [item_details] VARCHAR(512) NOT NULL,
+  [has_menu_icon] BIT NOT NULL,
+  [menu_icon_file] VARCHAR(50) NOT NULL,
+  PRIMARY KEY ([menu_dip_id])
+);
