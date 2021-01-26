150681e54d68b165c17b3bf68912217f4cb20028
Michael Rees <mrees791@gmail.com>
2021-01-26 14:53:51 -0500

    Added fields to user_address table.

--- PizzaDatabase/dbo/tables/user_address.sql
+++ PizzaDatabase/dbo/tables/user_address.sql
@@ -1,4 +1,14 @@
 ﻿CREATE TABLE [dbo].[user_address]
 (
-	[Id] INT NOT NULL PRIMARY KEY
-)
+  [address_id] INT NOT NULL IDENTITY,
+  [user_id] INT NOT NULL,
+  [address_type] VARCHAR(50) NOT NULL,
+  [address_name] VARCHAR(50) NOT NULL,
+  [street_address] VARCHAR(50) NOT NULL,
+  [city] VARCHAR(50) NOT NULL,
+  [state] VARCHAR(50) NOT NULL,
+  [zip_code] VARCHAR(5) NOT NULL,
+  [phone_number] VARCHAR(10) NOT NULL,
+  PRIMARY KEY ([address_id]),
+  FOREIGN KEY ([user_id]) REFERENCES [dbo].[user](user_id),
+);
