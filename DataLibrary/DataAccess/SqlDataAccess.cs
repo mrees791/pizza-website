ac18ebec72f86cdb57c35fed33a5bdef0aa0c6ab
Michael Rees <mrees791@gmail.com>
2021-01-26 17:37:23 -0500

    Added GetConnectionString, LoadData, SaveData methods to SqlDataAccess.

--- DataLibrary/DataAccess/SqlDataAccess.cs
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DataLibrary.DataAccess
 {
     public class SqlDataAccess
     {
+        public string GetConnectiongString(string connectionName = "PizzaDatabase")
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        public List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectiongString()))
+            {
+                return cnn.Query<T>(sql).ToList();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="data"></param>
+        /// <returns>Number of rows effected.</returns>
+        public int SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectiongString()))
+            {
+                return cnn.Execute(sql, data);
+            }
+        }
     }
 }
