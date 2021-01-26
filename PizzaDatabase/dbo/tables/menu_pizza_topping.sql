58e1438c0bbcfb1925ba9a633544516bbd87f44a
Michael Rees <mrees791@gmail.com>
2021-01-26 14:30:31 -0500

    Added fields to menu_pizza_topping table.

--- PizzaDatabase/dbo/tables/menu_pizza_topping.sql
+++ PizzaDatabase/dbo/tables/menu_pizza_topping.sql
@@ -1,4 +1,16 @@
 ﻿CREATE TABLE [dbo].[menu_pizza_topping]
 (
-	[Id] INT NOT NULL PRIMARY KEY
-)
+  [menu_pizza_topping_id] INT NOT NULL IDENTITY,
+  [available_for_purchase] BIT NOT NULL,
+  [name] VARCHAR(100) NOT NULL,
+  [price_light] DECIMAL(20,2) NOT NULL,
+  [price_regular] DECIMAL(20,2) NOT NULL,
+  [price_extra] DECIMAL(20,2) NOT NULL,
+  [pizza_topping_type] VARCHAR(50) NOT NULL,
+  [description] VARCHAR(512) NOT NULL,
+  [has_menu_icon] BIT NOT NULL,
+  [menu_icon_file] VARCHAR(50) NOT NULL,
+  [has_pizza_builder_image] BIT NOT NULL,
+  [pizza_builder_image_file] VARCHAR(50) NOT NULL,
+  PRIMARY KEY ([menu_pizza_topping_id])
+);
