874df82f37ea003aadf1aa4f75a96fef0cf0bf41
Michael Rees <mrees791@gmail.com>
2021-01-26 14:48:18 -0500

    Added fields to pizza_category table.

--- PizzaDatabase/dbo/tables/pizza_category.sql
+++ PizzaDatabase/dbo/tables/pizza_category.sql
@@ -1,4 +1,11 @@
 ﻿CREATE TABLE [dbo].[pizza_category]
 (
-	[Id] INT NOT NULL PRIMARY KEY
-)
+  [pizza_category_id] INT NOT NULL IDENTITY,
+  [pizza_id] INT NOT NULL,
+  [pizza_category_name] VARCHAR(50) NOT NULL,
+  [available_for_purchase] BIT NOT NULL,
+  [pizza_name] VARCHAR(100) NOT NULL,
+  [description] VARCHAR(512) NOT NULL,
+  PRIMARY KEY ([pizza_category_id]),
+  FOREIGN KEY ([pizza_id]) REFERENCES pizza(pizza_id)
+);
