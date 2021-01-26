da643e229b7d7916e2c34d9582031acba57c833c
Michael Rees <mrees791@gmail.com>
2021-01-26 14:55:35 -0500

    Added fields to user_role table.

--- PizzaDatabase/dbo/tables/user_address.sql
+++ PizzaDatabase/dbo/tables/user_address.sql
@@ -10,5 +10,5 @@
   [zip_code] VARCHAR(5) NOT NULL,
   [phone_number] VARCHAR(10) NOT NULL,
   PRIMARY KEY ([address_id]),
-  FOREIGN KEY ([user_id]) REFERENCES [dbo].[user](user_id),
+  FOREIGN KEY ([user_id]) REFERENCES [dbo].[user](user_id)
 );
--- PizzaDatabase/dbo/tables/user_role.sql
+++ PizzaDatabase/dbo/tables/user_role.sql
@@ -1,4 +1,8 @@
 ﻿CREATE TABLE [dbo].[user_role]
 (
-	[Id] INT NOT NULL PRIMARY KEY
-)
+  [user_role_id] INT NOT NULL IDENTITY,
+  [user_id] INT NOT NULL,
+  [role_name] VARCHAR(50) NOT NULL,
+  PRIMARY KEY ([user_role_id]),
+  FOREIGN KEY ([user_id]) REFERENCES [dbo].[user](user_id)
+);
