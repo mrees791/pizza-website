ae6e07f022645a467c9aeb35d34c83713bcc2368
Michael Rees <mrees791@gmail.com>
2021-01-26 20:13:22 -0500

    Created MenuProcessor.

---
+++ DataLibrary/BusinessLogic/MenuProcessor.cs
@@ -0,0 +1,32 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class MenuProcessor
+    {
+        public static int CreateMenuDip(bool availableForPurchase, string name,
+            decimal price, string itemDetails, bool hasMenuIcon, string menuIconFile)
+        {
+            MenuDipModel data = new MenuDipModel
+            {
+                AvailableForPurchase = availableForPurchase,
+                Name = name,
+                Price = price,
+                ItemDetails = itemDetails,
+                HasMenuIcon = hasMenuIcon,
+                MenuIconFile = menuIconFile
+            };
+
+            string sql = @"insert into dbo.menu_dip (available_for_purchase, name, price, item_details, has_menu_icon, menu_icon_file)
+                        values (@AvailableForPurchase, @Name, @Price, @ItemDetails, @HasMenuIcon, @MenuIconFile);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+    }
+}
---
+++ DataLibrary/Models/Menu/MenuDipModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models.Menu
+{
+    public class MenuDipModel
+    {
+        public int MenuDipId { get; set; }
+        public bool AvailableForPurchase { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ItemDetails { get; set; }
+        public bool HasMenuIcon { get; set; }
+        public string MenuIconFile { get; set; }
+    }
+}
