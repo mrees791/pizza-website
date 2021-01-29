1561278f647d8eb3fa3d5bb4844ef91fa0fd8a20
Michael Rees <mrees791@gmail.com>
2021-01-29 12:49:38 -0500

    Created models for menu pizza items.

---
+++ DataLibrary/Models/Menu/Pizza/MenuPizzaCrustModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models.Menu.Pizza
+{
+    public class MenuPizzaCrustModel : MenuPizzaItemModel
+    {
+        public decimal PriceSmall { get; set; }
+        public decimal PriceMedium { get; set; }
+        public decimal PriceLarge { get; set; }
+    }
+}
---
+++ DataLibrary/Models/Menu/Pizza/MenuPizzaItemModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models.Menu.Pizza
+{
+    public abstract class MenuPizzaItemModel : MenuItemModel
+    {
+        public bool HasPizzaBuilderImage { get; set; }
+        public string PizzaBuilderImageFile { get; set; }
+        public string Description { get; set; }
+    }
+}
