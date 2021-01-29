e54d6c88f333220a88d78eb38f0986ecb8dac194
Michael Rees <mrees791@gmail.com>
2021-01-29 17:43:04 -0500

    Updated comments.

--- DataLibrary/DataAccess/SqlDataAccess.cs
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -17,6 +17,12 @@ namespace DataLibrary.DataAccess
             return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
         }
 
+        /// <summary>
+        /// Loads a list of records from the database.
+        /// </summary>
+        /// <typeparam name="T">Model data-type.</typeparam>
+        /// <param name="sql">Select SQL query.</param>
+        /// <returns>List of records.</returns>
         public static List<T> LoadData<T>(string sql)
         {
             using (IDbConnection cnn = new SqlConnection(GetConnectiongString()))
@@ -26,11 +32,11 @@ namespace DataLibrary.DataAccess
         }
 
         /// <summary>
-        /// 
+        /// Updates a record using an updated model.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="sql"></param>
-        /// <param name="data"></param>
+        /// <typeparam name="T">Model data-type.</typeparam>
+        /// <param name="sql">Update SQL query.</param>
+        /// <param name="data">Updated model.</param>
         /// <returns>Number of rows affected.</returns>
         public static int UpdateRecord<T>(string sql, T data)
         {
@@ -44,9 +50,9 @@ namespace DataLibrary.DataAccess
         /// Adds a new record using an SQL insert query and returns the ID of the newly created record.
         /// The insert query must include "output Inserted.Id" in order to return the newly created ID.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="sql"></param>
-        /// <param name="data"></param>
+        /// <typeparam name="T">Model data-type.</typeparam>
+        /// <param name="sql">Insert SQL query.</param>
+        /// <param name="data">Model.</param>
         /// <returns>Id of newly created record.</returns>
         public static int SaveNewRecord<T>(string sql, T data)
         {
