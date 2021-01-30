fa2aeacd69ec5ee1713cc856827d5507b1234118
Michael Rees <mrees791@gmail.com>
2021-01-30 15:32:19 -0500

    Added LoadMenuPizzaToppings, UpdateMenuPizzaTopping, AddMenuPizzaTopping methods.

--- DataLibrary/BusinessLogic/DatabaseMenuPizzaProcessor.cs
+++ DataLibrary/BusinessLogic/DatabaseMenuPizzaProcessor.cs
@@ -10,6 +10,61 @@ namespace DataLibrary.BusinessLogic
 {
     public static class DatabaseMenuPizzaProcessor
     {
+        public static List<MenuPizzaToppingModel> LoadMenuPizzaToppings()
+        {
+            string sql = @"select Id, AvailableForPurchase, Name, PriceLight, PriceRegular, PriceExtra,
+                           PizzaToppingType, Description, HasMenuIcon, MenuIconFile, HasPizzaBuilderImage, PizzaBuilderImageFile from dbo.MenuPizzaTopping;";
+
+            return SqlDataAccess.LoadData<MenuPizzaToppingModel>(sql);
+        }
+
+        public static int UpdateMenuPizzaTopping(
+            int id,
+            bool availableForPurchase,
+            string name,
+            decimal priceLight,
+            decimal priceRegular,
+            decimal priceExtra,
+            string pizzaToppingType,
+            string description,
+            bool hasMenuIcon,
+            string menuIconFile,
+            bool hasPizzaBuilderImage,
+            string pizzaBuilderImageFile)
+        {
+            MenuPizzaToppingModel data = new MenuPizzaToppingModel(id, availableForPurchase, name, hasMenuIcon, menuIconFile, hasPizzaBuilderImage, pizzaBuilderImageFile, description,
+                priceLight, priceRegular, priceExtra, pizzaToppingType);
+
+            string sql = @"update dbo.MenuPizzaTopping set AvailableForPurchase = @AvailableForPurchase, Name = @Name, PriceLight = @PriceLight,
+                           PriceRegular = @PriceRegular, PriceExtra = @PriceExtra, PizzaToppingType = @PizzaToppingType, Description = @Description,
+                           HasMenuIcon = @HasMenuIcon, MenuIconFile = @MenuIconFile, HasPizzaBuilderImage = @HasPizzaBuilderImage, PizzaBuilderImageFile = @PizzaBuilderImageFile where Id = @Id;";
+
+            return SqlDataAccess.UpdateRecord(sql, data);
+        }
+
+        public static int AddMenuPizzaTopping(bool availableForPurchase,
+            string name,
+            decimal priceLight,
+            decimal priceRegular,
+            decimal priceExtra,
+            string pizzaToppingType,
+            string description,
+            bool hasMenuIcon,
+            string menuIconFile,
+            bool hasPizzaBuilderImage,
+            string pizzaBuilderImageFile)
+        {
+            MenuPizzaToppingModel data = new MenuPizzaToppingModel(0, availableForPurchase, name, hasMenuIcon, menuIconFile, hasPizzaBuilderImage, pizzaBuilderImageFile, description,
+                priceLight, priceRegular, priceExtra, pizzaToppingType);
+
+            string sql = @"insert into dbo.MenuPizzaTopping (AvailableForPurchase, Name, PriceLight, PriceRegular, PriceExtra,
+                           PizzaToppingType, Description, HasMenuIcon, MenuIconFile, HasPizzaBuilderImage, PizzaBuilderImageFile)
+                           output Inserted.Id values (@AvailableForPurchase, @Name, @PriceLight, @PriceRegular, @PriceExtra,
+                           @PizzaToppingType, @Description, @HasMenuIcon, @MenuIconFile, @HasPizzaBuilderImage, @PizzaBuilderImageFile);";
+
+            return SqlDataAccess.SaveNewRecord(sql, data);
+        }
+
         public static List<MenuPizzaSauceModel> LoadMenuPizzaSaucees()
         {
             string sql = @"select Id, AvailableForPurchase, Name, PriceLight, PriceRegular, PriceExtra,
