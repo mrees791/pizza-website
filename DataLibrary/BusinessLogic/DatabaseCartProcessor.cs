8817ee7d8088ddb5d82c16f353e8fd9863bb6886
Michael Rees <mrees791@gmail.com>
2021-02-03 14:34:04 -0500

    Implemented overloaded AddPizzaToCart method.

--- DataLibrary/BusinessLogic/DatabaseCartProcessor.cs
+++ DataLibrary/BusinessLogic/DatabaseCartProcessor.cs
@@ -5,6 +5,7 @@ using DataLibrary.Models.Pizzas;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,19 +14,45 @@ namespace DataLibrary.BusinessLogic
 {
     public static class DatabaseCartProcessor
     {
+        public static int AddPizzaToCart(int cartId, PizzaModel pizzaModel, int quantity)
+        {
+            int cartPizzaId = 0;
+
+            using (IDbConnection connection = new SqlConnection(SqlDataAccess.GetConnectiongString()))
+            {
+                connection.Open();
+
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        cartPizzaId = AddPizzaToCart(cartId, pizzaModel, quantity, connection, transaction);
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+
+            return cartPizzaId;
+        }
+
         internal static int AddPizzaToCart(int cartId, PizzaModel pizzaModel, int quantity, IDbConnection connection, IDbTransaction transaction)
         {
             string insertSql = @"insert into dbo.CartPizza (CartId, PizzaId, PricePerItem, Quantity) output Inserted.Id values(@CartId, @MenuDessertId, @PricePerItem, @Quantity);";
 
             // Save pizza record
-            int newPizzaId = DatabasePizzaProcessor.AddPizza(pizzaModel, connection, transaction);
+            pizzaModel.Id = DatabasePizzaProcessor.AddPizza(pizzaModel, connection, transaction);
 
             // Save cart pizza record
             return SqlDataAccess.SaveNewRecord(insertSql,
                 new
                 {
                     CartId = cartId,
-                    PizzaId = newPizzaId,
+                    PizzaId = pizzaModel.Id,
                     PricePerItem = pizzaModel.GetPrice(),
                     Quantity = quantity
                 }
