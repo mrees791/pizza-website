3a19265b348f06c9ecbefd45c113828203e55f78
Michael Rees <mrees791@gmail.com>
2021-02-03 17:18:32 -0500

    Removed pricePerItem parameters from cart methods.

--- DataLibrary/BusinessLogic/DatabaseCartProcessor.cs
+++ DataLibrary/BusinessLogic/DatabaseCartProcessor.cs
@@ -59,6 +59,97 @@ namespace DataLibrary.BusinessLogic
             return SqlDataAccess.SaveNewRecord(insertSql, queryParameters, connection, transaction);
         }
 
+        internal static int AddDrinkToCart(int cartId, MenuDrinkModel menuDrink, string size, int quantity, IDbConnection connection, IDbTransaction transaction)
+        {
+            string insertSql = @"insert into dbo.CartDrink (CartId, MenuDrinkId, PricePerItem, Size, Quantity)
+                                 output Inserted.Id values(@CartId, @MenuDrinkId, @PricePerItem, @Size, @Quantity);";
+
+            decimal pricePerItem = 0.0m;
+
+            switch (size)
+            {
+                case DrinkSizes.Size20Oz:
+                    pricePerItem = menuDrink.Price20Oz;
+                    break;
+                case DrinkSizes.Size2Liter:
+                    pricePerItem = menuDrink.Price2Liter;
+                    break;
+                case DrinkSizes.Size2Pack12Oz:
+                    pricePerItem = menuDrink.Price2Pack12Oz;
+                    break;
+                case DrinkSizes.Size6Pack12Oz:
+                    pricePerItem = menuDrink.Price6Pack12Oz;
+                    break;
+                default:
+                    throw new Exception($"Drink size not found: {size}");
+            }
+
+            object queryParameters = new
+            {
+                CartId = cartId,
+                MenuDrinkId = menuDrink.Id,
+                PricePerItem = pricePerItem,
+                Size = size,
+                Quantity = quantity
+            };
+
+            // Save cart drink record
+            return SqlDataAccess.SaveNewRecord(insertSql, queryParameters, connection, transaction);
+        }
+
+        internal static int AddWingsToCart(int cartId, MenuWingsModel menuWings, MenuWingsSauceModel menuWingsSauce, int pieceAmount, int quantity, IDbConnection connection, IDbTransaction transaction)
+        {
+            string insertSql = @"insert into dbo.CartWings (CartId, MenuWingsId, MenuWingsSauceId, PieceAmount, PricePerItem, Quantity)
+                                 output Inserted.Id values(@CartId, @MenuWingsId, @MenuWingsSauceId, @PieceAmount, @PricePerItem, @Quantity);";
+
+            decimal pricePerItem = 0.0m;
+
+            switch(pieceAmount)
+            {
+                case 6:
+                    pricePerItem = menuWings.Price6Piece;
+                    break;
+                case 12:
+                    pricePerItem = menuWings.Price12Piece;
+                    break;
+                case 18:
+                    pricePerItem = menuWings.Price18Piece;
+                    break;
+                default:
+                    throw new Exception($"Price not found for wing amount: {pieceAmount}");
+            }
+
+            object queryParameters = new
+            {
+                CartId = cartId,
+                MenuWingsId = menuWings.Id,
+                MenuWingsSauceId = menuWingsSauce.Id,
+                PieceAmount = pieceAmount,
+                PricePerItem = pricePerItem,
+                Quantity = quantity
+            };
+
+            // Save cart wings record
+            return SqlDataAccess.SaveNewRecord(insertSql, queryParameters, connection, transaction);
+        }
+
+        internal static int AddDipToCart(int cartId, MenuDipModel menuDip, int quantity, IDbConnection connection, IDbTransaction transaction)
+        {
+            string insertSql = @"insert into dbo.CartDip (CartId, MenuDipId, PricePerItem, Quantity)
+                                 output Inserted.Id values(@CartId, @MenuDipId, @PricePerItem, @Quantity);";
+
+            object queryParameters = new
+            {
+                CartId = cartId,
+                MenuDipId = menuDip.Id,
+                PricePerItem = menuDip.Price,
+                Quantity = quantity
+            };
+
+            // Save cart dip record
+            return SqlDataAccess.SaveNewRecord(insertSql, queryParameters, connection, transaction);
+        }
+
         internal static int AddDessertToCart(int cartId, MenuDessertModel menuDessert, int quantity, IDbConnection connection, IDbTransaction transaction)
         {
             string insertSql = @"insert into dbo.CartDessert (CartId, MenuDessertId, PricePerItem, Quantity)
@@ -76,21 +167,20 @@ namespace DataLibrary.BusinessLogic
             return SqlDataAccess.SaveNewRecord(insertSql, queryParameters, connection, transaction);
         }
 
-        internal static int AddDrinkToCart(int cartId, MenuDrinkModel menuDrink, decimal pricePerItem, string size, int quantity, IDbConnection connection, IDbTransaction transaction)
+        internal static int AddPastaToCart(int cartId, MenuPastaModel menuPasta, int quantity, IDbConnection connection, IDbTransaction transaction)
         {
-            string insertSql = @"insert into dbo.CartDrink (CartId, MenuDrinkId, PricePerItem, Size, Quantity)
-                                 output Inserted.Id values(@CartId, @MenuDrinkId, @PricePerItem, @Size, @Quantity);";
+            string insertSql = @"insert into dbo.CartPasta (CartIt, MenuPastaId, PricePerItem, Quantity)
+                                 output Inserted.Id values(@CartIt, @MenuPastaId, @PricePerItem, @Quantity);";
 
             object queryParameters = new
             {
                 CartId = cartId,
-                MenuDrinkId = menuDrink.Id,
-                PricePerItem = pricePerItem,
-                Size = size,
+                MenuPastaId = menuPasta.Id,
+                PricePerItem = menuPasta.Price,
                 Quantity = quantity
             };
 
-            // Save cart drink record
+            // Save cart pasta record
             return SqlDataAccess.SaveNewRecord(insertSql, queryParameters, connection, transaction);
         }
 
---
+++ DataLibrary/Models/Menu/DrinkSizes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models.Menu
+{
+    public class DrinkSizes
+    {
+        public const string Size20Oz = "20 Oz";
+        public const string Size2Liter = "2 Liter";
+        public const string Size2Pack12Oz = "2 Pack 12 Oz";
+        public const string Size6Pack12Oz = "6 Pack 12 Oz";
+    }
+}
