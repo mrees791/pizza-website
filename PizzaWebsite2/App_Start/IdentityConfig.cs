3690c122765b3bb18feaf7c99ee016ce94650a3a
Michael Rees <mrees791@gmail.com>
2021-02-25 16:37:11 -0500

    Added reference to AppSettingsSecrets.config to PizzaWebsite2.

--- PizzaWebsite2/App_Start/IdentityConfig.cs
+++ PizzaWebsite2/App_Start/IdentityConfig.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
@@ -15,10 +19,38 @@ namespace PizzaWebsite2
 {
     public class EmailService : IIdentityMessageService
     {
-        public Task SendAsync(IdentityMessage message)
+        public async Task SendAsync(IdentityMessage message)
         {
-            // Plug in your email service here to send an email.
-            return Task.FromResult(0);
+            await ConfigSendAsync(message);
+        }
+
+        private async Task ConfigSendAsync(IdentityMessage message)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient smtpServer = new SmtpClient(ConfigurationManager.AppSettings["smtpHost"]);
+
+            mail.From = new MailAddress(ConfigurationManager.AppSettings["smtpEmailAddress"]);
+            mail.To.Add(message.Destination);
+            mail.Subject = message.Subject;
+            mail.Body = message.Body;
+            mail.IsBodyHtml = true;
+
+            smtpServer.Port = int.Parse(ConfigurationManager.AppSettings["smtpPort"]);
+            smtpServer.Credentials = new NetworkCredential(
+                ConfigurationManager.AppSettings["smtpEmailAddress"],
+                ConfigurationManager.AppSettings["smtpPassword"]);
+            // todo: Enable SSL on production server.
+            //smtpServer.EnableSsl = true;
+
+            try
+            {
+                await smtpServer.SendMailAsync(mail);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError($"Error sending email to {message.Destination}.\n{ex.StackTrace}");
+                await Task.FromResult(0);
+            }
         }
     }
 
