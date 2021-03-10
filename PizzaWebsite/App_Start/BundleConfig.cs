714f26c0e349bcf1412c5da93c11497642ea7379
Michael Rees <mrees791@gmail.com>
2021-03-10 18:09:27 -0500

    Finished createPagination JS method.

--- PizzaWebsite/App_Start/BundleConfig.cs
+++ PizzaWebsite/App_Start/BundleConfig.cs
@@ -9,11 +9,13 @@ namespace PizzaWebsite
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                      "~/Scripts/jquery.paging.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryplugins").Include(
+                      "~/Scripts/jquery.paging.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -22,6 +24,9 @@ namespace PizzaWebsite
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/site").Include(
+                      "~/Scripts/site.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
