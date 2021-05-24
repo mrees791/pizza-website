e6da011b17c355615ec3a28f09daf8ad448b2e1b
Michael Rees <mrees791@gmail.com>
2021-05-24 14:40:17 -0400

    Fixed cartIsEmpty JS variable so if the cart table only has the subtotal row, then the cart is considered empty.

--- PizzaWebsite/Scripts/site-cart.js
+++ PizzaWebsite/Scripts/site-cart.js
@@ -12,15 +12,20 @@ const pizzaSiteCartNs = {
         });
     },
     updateCartElementVisibility: () => {
+        var $cartTableBody = $('#cartTableBody');
+        var $cartBlock = $('#cartBlock');
+        var $emptyCartBlock = $('#emptyCartBlock');
+
         // Get number of rows in table
-        var numberOfCartItems = $('#cartTableBody').children().length;
-        var cartIsEmpty = numberOfCartItems === 0;
+        var numberOfCartItems = $cartTableBody.children().length;
+        // If it only has the subtotal row, then the cart is empty;
+        var cartIsEmpty = numberOfCartItems === 1;
         if (cartIsEmpty) {
-            $('#cartBlock').hide();
-            $('#emptyCartBlock').show();
+            $cartBlock.hide();
+            $emptyCartBlock.show();
         } else {
-            $('#emptyCartBlock').hide();
-            $('#cartBlock').show();
+            $emptyCartBlock.hide();
+            $cartBlock.show();
         }
     }
 };
