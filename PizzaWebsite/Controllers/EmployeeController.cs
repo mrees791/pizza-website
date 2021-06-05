b1c645f2a97058edd850de8cb3d0a5743f6773ed
Michael Rees <mrees791@gmail.com>
2021-06-05 15:28:52 -0400

    Implemented SearchUsers view for Employee controller.

--- PizzaWebsite/Controllers/EmployeeController.cs
+++ PizzaWebsite/Controllers/EmployeeController.cs
@@ -1,4 +1,7 @@
-﻿using DataLibrary.Models.Tables;
+﻿using DataLibrary.Models;
+using DataLibrary.Models.QueryFilters;
+using DataLibrary.Models.Tables;
+using PizzaWebsite.Models;
 using PizzaWebsite.Models.Employees;
 using System;
 using System.Collections.Generic;
@@ -17,6 +20,32 @@ namespace PizzaWebsite.Controllers
             SiteUser currentUser = await GetCurrentUserAsync();
             EmployeeIndexViewModel model = new EmployeeIndexViewModel();
             await model.InitializeAsync(currentUser, PizzaDb);
+
+            return View(model);
+        }
+
+        public async Task<ActionResult> SearchUsers(int? page, int? rowsPerPage, string userId, string email)
+        {
+            ManagePagedListViewModel<SearchUserViewModel> model = new ManagePagedListViewModel<SearchUserViewModel>();
+
+            SiteUserFilter searchFilter = new SiteUserFilter()
+            {
+                Id = userId,
+                Email = email
+            };
+
+            List<SiteUser> userList = await LoadPagedRecordsAsync<SiteUser>(page, rowsPerPage, "Id", SortOrder.Ascending, searchFilter, PizzaDb, Request, model.PaginationVm);
+
+            foreach (SiteUser user in userList)
+            {
+                SearchUserViewModel userVm = new SearchUserViewModel()
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    IsBanned = user.IsBanned
+                };
+                model.ItemViewModelList.Add(userVm);
+            }
 
             return View(model);
         }
