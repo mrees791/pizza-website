95cbeb5373b9eb178321c10c99471b4a0af83ff5
Michael Rees <mrees791@gmail.com>
2021-06-08 15:31:04 -0400

    Finished organizing ManageUsers models.

--- PizzaWebsite/Models/ManageUsers/ManageUserViewModel.cs
+++ PizzaWebsite/Models/ManageUsers/ManageUserViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace PizzaWebsite.Models.Employees
+namespace PizzaWebsite.Models.ManageUsers
 {
     public class ManageUserViewModel
     {
