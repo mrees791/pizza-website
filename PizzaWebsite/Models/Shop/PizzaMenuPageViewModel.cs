e481b563330b7fa12f1fec9006963a1099d069a8
Michael Rees <mrees791@gmail.com>
2021-06-15 14:33:17 -0400

    Removed constructor from PizzaMenuPageViewModel. Changed list properties to IEnumerable.

--- PizzaWebsite/Models/Shop/PizzaMenuPageViewModel.cs
+++ PizzaWebsite/Models/Shop/PizzaMenuPageViewModel.cs
@@ -7,15 +7,8 @@ namespace PizzaWebsite.Models.Shop
 {
     public class PizzaMenuPageViewModel
     {
-        public List<MenuPizzaViewModel> PopularPizzaList { get; set; }
-        public List<MenuPizzaViewModel> MeatsPizzaList { get; set; }
-        public List<MenuPizzaViewModel> VeggiePizzaList { get; set; }
-
-        public PizzaMenuPageViewModel()
-        {
-            PopularPizzaList = new List<MenuPizzaViewModel>();
-            MeatsPizzaList = new List<MenuPizzaViewModel>();
-            VeggiePizzaList = new List<MenuPizzaViewModel>();
-        }
+        public IEnumerable<MenuPizzaViewModel> PopularPizzaList { get; set; }
+        public IEnumerable<MenuPizzaViewModel> MeatsPizzaList { get; set; }
+        public IEnumerable<MenuPizzaViewModel> VeggiePizzaList { get; set; }
     }
 }
