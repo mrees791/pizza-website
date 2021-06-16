795088ff0589eea9e9359e877aad7dba46dde028
Michael Rees <mrees791@gmail.com>
2021-06-16 12:28:16 -0400

    Implemented ManageOrders Index.

--- PizzaWebsite/Controllers/ManageOrdersController.cs
+++ PizzaWebsite/Controllers/ManageOrdersController.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using DataLibrary.Models.QueryFilters;
+using DataLibrary.Models.Tables;
+using Microsoft.AspNet.Identity;
 using PizzaWebsite.Controllers.BaseControllers;
+using PizzaWebsite.Models;
 using PizzaWebsite.Models.ManageOrders;
+using PizzaWebsite.Models.ManageStores;
+using PizzaWebsite.Models.Services;
 using PizzaWebsite.Models.ViewModelServices;
 
 namespace PizzaWebsite.Controllers
@@ -16,21 +23,50 @@ namespace PizzaWebsite.Controllers
     public class ManageOrdersController : BaseController
     {
         private readonly CustomerOrderServices _customerOrderServices;
+        private readonly StoreServices _storeServices;
 
         public ManageOrdersController()
         {
             _customerOrderServices = new CustomerOrderServices();
+            _storeServices = new StoreServices();
         }
 
-        public ActionResult Index(int? page, int? rowsPerPage)
+        public async Task<ActionResult> Index(int? page, int? rowsPerPage, string storeName, string phoneNumber)
         {
             ValidatePageQuery(ref page, ref rowsPerPage, 10);
+            StoreLocationFilter searchFilter = new StoreLocationFilter
+            {
+                Name = storeName,
+                PhoneNumber = phoneNumber
+            };
 
             // Load authorized store list.
+            SiteUser currentUser = await GetCurrentUserAsync();
+            Employee currentEmployee = await PizzaDb.GetEmployeeAsync(currentUser);
+            PaginationViewModel paginationVm = new PaginationViewModel();
+            List<StoreViewModel> storeVmList = new List<StoreViewModel>();
+            IList<string> userRoleList = await UserManager.GetRolesAsync(User.Identity.GetUserId());
+            IEnumerable<StoreLocation> storeList = await _storeServices.LoadAuthorizedStoreLocationListAsync(page.Value, rowsPerPage.Value, userRoleList,
+                searchFilter, paginationVm, currentEmployee, Request, PizzaDb);
+
+            foreach (StoreLocation store in storeList)
+            {
+                storeVmList.Add(new StoreViewModel()
+                {
+                    Id = store.Id,
+                    Name = store.Name,
+                    City = store.City,
+                    PhoneNumber = store.PhoneNumber,
+                    StreetAddress = store.StreetAddress,
+                    ZipCode = store.ZipCode,
+                    IsActiveLocation = store.IsActiveLocation
+                });
+            }
 
             ManageOrdersIndexViewModel model = new ManageOrdersIndexViewModel()
             {
-
+                StoreVmList = storeVmList,
+                PaginationVm = paginationVm
             };
 
             return View(model);
