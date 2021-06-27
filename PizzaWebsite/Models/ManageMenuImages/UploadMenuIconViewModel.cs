a7f68426f367e3a9605428df84b8c1c515b6a739
Michael Rees <mrees791@gmail.com>
2021-06-27 15:05:15 -0400

    Revert "Last commit with BlueImp jQuery File Upload."
    This reverts commit 27f21933a5bce58d1daa6c3d55006fcb82f310cd.

---
+++ PizzaWebsite/Models/ManageMenuImages/UploadMenuIconViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace PizzaWebsite.Models.ManageMenuImages
+{
+    public class UploadMenuIconViewModel
+    {
+        public int Id { get; set; }
+    }
+}
