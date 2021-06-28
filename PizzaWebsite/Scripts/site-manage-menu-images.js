02c8d39a9381868b103f9f67e6a369c14e39d642
Michael Rees <mrees791@gmail.com>
2021-06-28 14:36:34 -0400

    Created _MenuIconForm partial view with initializeManageMenuIconSection JS method.

--- PizzaWebsite/Scripts/site-manage-menu-images.js
+++ PizzaWebsite/Scripts/site-manage-menu-images.js
@@ -3,5 +3,27 @@ const pizzaSiteManageMenuImageNs = {
     updateImageElement: ($img) => {
         var imgSrc = $img.attr('src');
         $img.attr('src', imgSrc + '?dateTime=' + new Date().getTime());
+    },
+    initializeManageMenuIconSection: (actionUrl) => {
+        var $menuIconImg = $('#menuIcon');
+        var $menuIconError = $('#menuIconError');
+        var $menuIconDropArea = $('#menuIconDropArea');
+        $menuIconDropArea.dmUploader({
+            url: actionUrl,
+            onInit: function () { },
+            onNewFile: function (id, file) { },
+            onBeforeUpload: function (id) { },
+            onUploadProgress: function (id, percent) { },
+            onUploadSuccess: function (id, data) {
+                console.log(data);
+                $menuIconError.hide();
+                pizzaSiteManageMenuImageNs.updateImageElement($menuIconImg);
+            },
+            onUploadError: function (id, xhr, status, errorThrown) {
+                console.log(xhr);
+                $menuIconError.text(xhr.responseText.replaceAll('"', ''));
+                $menuIconError.show();
+            }
+        });
     }
 };
