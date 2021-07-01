1c2c90ba99ffad2a25e89a1bae632d89986a4041
Michael Rees <mrees791@gmail.com>
2021-07-01 16:38:58 -0400

    Fixed pizza builder image now working without main navbar.

--- PizzaWebsite/Scripts/site-pizza-builder.js
+++ PizzaWebsite/Scripts/site-pizza-builder.js
@@ -1,6 +1,21 @@
 ﻿// Namespace for pizza builder view scripts
 const pizzaSitePizzaBuilderNs = {
-    testAlert: () => {
-        alert('test!!!')
+    initializePizzaBuilder: () => {
+        var $mainNavbar = $('#main-navbar');
+        var $header = $('#pb-header');
+        var $body = $('body');
+        var $mainContainer = $('#main-container');
+        var $pizzaBuilder = $('#pizza-builder');
+        var $menuSection = $('#pizza-builder-menu-section');
+        var $imageSection = $('#pizza-builder-image-section');
+        var pbOffset = $pizzaBuilder.offset().top;
+        $(window).scroll(function () {
+            var currentTop = $(document).scrollTop();
+            if (currentTop >= pbOffset) {
+                $imageSection.css('margin-top', currentTop - pbOffset);
+            } else {
+                $imageSection.css('margin-top', 0);
+            }
+        });
     }
 };
