6aba5190711765ecb25555a74682c6f34a12cc20
Michael Rees <mrees791@gmail.com>
2021-07-01 16:54:13 -0400

    Organized site-pizza-builder JS code.

--- PizzaWebsite/Scripts/site-pizza-builder.js
+++ PizzaWebsite/Scripts/site-pizza-builder.js
@@ -2,17 +2,16 @@
 const pizzaSitePizzaBuilderNs = {
     initializePizzaBuilder: () => {
         var $mainNavbar = $('#main-navbar');
-        var $header = $('#pb-header');
-        var $body = $('body');
-        var $mainContainer = $('#main-container');
         var $pizzaBuilder = $('#pizza-builder');
-        var $menuSection = $('#pizza-builder-menu-section');
         var $imageSection = $('#pizza-builder-image-section');
-        var pbOffset = $pizzaBuilder.offset().top;
+
         $(window).scroll(function () {
             var currentTop = $(document).scrollTop();
-            if (currentTop >= pbOffset) {
-                $imageSection.css('margin-top', currentTop - pbOffset);
+            var pbOffset = $pizzaBuilder.offset().top;
+            var navbarHeight = $mainNavbar.height();
+            var marginOffset = pbOffset - navbarHeight;
+            if (currentTop >= marginOffset) {
+                $imageSection.css('margin-top', currentTop - marginOffset);
             } else {
                 $imageSection.css('margin-top', 0);
             }
