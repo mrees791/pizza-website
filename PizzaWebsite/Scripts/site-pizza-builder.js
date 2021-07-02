d663d7304b93a3b3201d4e6a140f0beef4be452d
Michael Rees <mrees791@gmail.com>
2021-07-02 17:27:08 -0400

    Removed unused pizza builder canvas JS code.

--- PizzaWebsite/Scripts/site-pizza-builder.js
+++ PizzaWebsite/Scripts/site-pizza-builder.js
@@ -1,26 +1,5 @@
 ﻿// Namespace for pizza builder view scripts
 const pizzaSitePizzaBuilderNs = {
-    updatePizzaBuilderCanvas: () => {
-        // Update selected crust
-        var $crustSelect = $('#crust-select');
-        var $selectedCrustOption = $crustSelect.find(':selected');
-        var selectedCrustImgSrc = $selectedCrustOption.attr('pb-img-src')
-
-        var canvas = document.getElementById('pizza-builder-canvas');
-        if (canvas.getContext) {
-            console.log('Canvas element is supported by this browser.');
-            var context = canvas.getContext('2d');
-
-            // Initialize layers
-            var crustImg = new Image();
-            crustImg.src = selectedCrustImgSrc;
-
-            // Draw layers
-            context.drawImage(crustImg, 0, 0);
-        } else {
-            console.log('Canvas element is not supported by this browser.');
-        }
-    },
     initializePizzaBuilder: () => {
         var $mainNavbar = $('#main-navbar');
         var $pizzaBuilder = $('#pizza-builder');
