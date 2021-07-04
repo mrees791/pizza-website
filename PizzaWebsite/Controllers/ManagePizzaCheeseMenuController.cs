f4529b72ce6c659e595de1c40c6cd8d088dff2ef
Michael Rees <mrees791@gmail.com>
2021-07-04 14:00:00 -0400

    Implemented manage image functionality for pizza cheese menu.

--- PizzaWebsite/Controllers/ManagePizzaCheeseMenuController.cs
+++ PizzaWebsite/Controllers/ManagePizzaCheeseMenuController.cs
@@ -3,6 +3,7 @@ using System.Web.Mvc;
 using DataLibrary.Models.QueryFilters;
 using DataLibrary.Models.Tables;
 using PizzaWebsite.Controllers.BaseControllers;
+using PizzaWebsite.Models;
 using PizzaWebsite.Models.ManageMenuImages;
 using PizzaWebsite.Models.ManageMenus;
 
@@ -13,11 +14,10 @@ namespace PizzaWebsite.Controllers
     {
         public ManagePizzaCheeseMenuController()
         {
-            PizzaBuilderImageValidation = new MenuImageValidation()
-            {
-                RequiredWidth = 100,
-                RequiredHeight = 50
-            };
+            MenuIconValidation.RequiredWidth = 100;
+            MenuIconValidation.RequiredHeight = 100;
+            PizzaBuilderImageValidation.RequiredWidth = 525;
+            PizzaBuilderImageValidation.RequiredHeight = 525;
         }
 
         public async Task<ActionResult> Index(int? page, int? rowsPerPage, string name)
@@ -52,6 +52,45 @@ namespace PizzaWebsite.Controllers
         public async Task<ActionResult> Edit(ManageMenuPizzaCheeseViewModel model)
         {
             return await Edit(model, model.Name);
+        }
+
+        public async Task<ActionResult> ManageImages(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return MissingIdErrorMessage();
+            }
+            MenuPizzaCheese record = await PizzaDb.GetAsync<MenuPizzaCheese>(id.Value);
+            if (record == null)
+            {
+                return InvalidIdErrorMessage(id.Value);
+            }
+            UploadMenuImageFormViewModel menuIconVm = new UploadMenuImageFormViewModel()
+            {
+                Name = "Menu Icon",
+                Description = $"This is the icon the user will click on when creating their pizza in the pizza builder. The dimensions must be {MenuIconValidation.RequiredWidth}x{MenuIconValidation.RequiredHeight}.",
+                ImageUrl = DirectoryServices.GetMenuImageUrl(record.Id, record.GetMenuCategoryType(), MenuImageType.MenuIcon),
+                DropAreaId = "menuIconDropArea",
+                ErrorMessageId = "menuIconError",
+                ImageId = "menuIcon"
+            };
+            UploadMenuImageFormViewModel pizzaBuilderImageVm = new UploadMenuImageFormViewModel()
+            {
+                Name = "Pizza Builder Image",
+                Description = $"This is the image that will be shown on the cheese layer of the pizza builder. The dimensions must be {PizzaBuilderImageValidation.RequiredWidth}x{PizzaBuilderImageValidation.RequiredHeight}.",
+                ImageUrl = DirectoryServices.GetMenuImageUrl(record.Id, record.GetMenuCategoryType(), MenuImageType.PizzaBuilderImage),
+                DropAreaId = "pizzaBuilderImageDropArea",
+                ErrorMessageId = "pizzaBuilderImageError",
+                ImageId = "pizzaBuilderImage"
+            };
+            ManagePizzaMenuIngredientImagesViewModel model = new ManagePizzaMenuIngredientImagesViewModel()
+            {
+                Id = id.Value,
+                ViewTitle = $"Manage Images - {record.Name}",
+                MenuIconVm = menuIconVm,
+                PizzaBuilderImageVm = pizzaBuilderImageVm
+            };
+            return View(model);
         }
 
         protected override async Task<ManageMenuPizzaCheeseViewModel> RecordToViewModelAsync(MenuPizzaCheese record)
