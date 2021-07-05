6372b4701c35b0b23da0304950a9e893f4fc81e3
Michael Rees <mrees791@gmail.com>
2021-07-05 13:50:02 -0400

    Finished crust radio button controls.

--- PizzaWebsite/Scripts/site-pizza-builder.js
+++ PizzaWebsite/Scripts/site-pizza-builder.js
@@ -15,7 +15,8 @@ const pizzaSitePizzaBuilderNs = {
         var $pizzaBuilder = $('#pizza-builder');
         var $imageSection = $('#pizza-builder-image-section');
 
-        $(window).scroll(function () {
+        // Disabled fixed pizza builder image.
+        /*$(window).scroll(function () {
             var currentTop = $(document).scrollTop();
             var pbOffset = $pizzaBuilder.offset().top;
             var navbarHeight = $mainNavbar.height();
@@ -25,7 +26,7 @@ const pizzaSitePizzaBuilderNs = {
             } else {
                 $imageSection.css('margin-top', 0);
             }
-        });
+        });*/
 
         // test crust fieldset
         var $crustFieldset = $('#crust-fieldset');
