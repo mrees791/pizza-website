f9b002106474a126fb00e5fcc23a99b093a3a811
Michael Rees <mrees791@gmail.com>
2021-07-06 14:08:55 -0400

    Revert "Added jQuery UI files to bundle file and included them in the layout file."
    This reverts commit 556318249ff05cbba857481aa466d123a07bd101.

--- PizzaWebsite/App_Start/BundleConfig.cs
+++ PizzaWebsite/App_Start/BundleConfig.cs
@@ -13,9 +13,6 @@ namespace PizzaWebsite
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                "~/Scripts/jquery-ui-{version}.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryplugins").Include(
                 "~/Scripts/jquery.paging.js",
                 "~/Scripts/jquery.dm-uploader.js"));
@@ -27,21 +24,6 @@ namespace PizzaWebsite
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js"));
-
-            // jQuery UI CSS
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                "~/Content/themes/base/jquery.ui.core.css",
-                "~/Content/themes/base/jquery.ui.resizable.css",
-                "~/Content/themes/base/jquery.ui.selectable.css",
-                "~/Content/themes/base/jquery.ui.accordion.css",
-                "~/Content/themes/base/jquery.ui.autocomplete.css",
-                "~/Content/themes/base/jquery.ui.button.css",
-                "~/Content/themes/base/jquery.ui.dialog.css",
-                "~/Content/themes/base/jquery.ui.slider.css",
-                "~/Content/themes/base/jquery.ui.tabs.css",
-                "~/Content/themes/base/jquery.ui.datepicker.css",
-                "~/Content/themes/base/jquery.ui.progressbar.css",
-                "~/Content/themes/base/jquery.ui.theme.css"));
 
             // jQuery DM Uploader CSS
             bundles.Add(new StyleBundle("~/Content/css").Include(
