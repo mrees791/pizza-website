e6dbcdd2b4cd8dbb0677eb7a9dcc8c62aff5048e
Michael Rees <mrees791@gmail.com>
2021-07-07 15:26:52 -0400

    Included all jQuery UI scripts and CSS in to bundle config. Included new bundles in _Layout.

--- PizzaWebsite/App_Start/BundleConfig.cs
+++ PizzaWebsite/App_Start/BundleConfig.cs
@@ -13,6 +13,9 @@ namespace PizzaWebsite
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            "~/Scripts/jquery-ui-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryplugins").Include(
                 "~/Scripts/jquery.paging.js",
                 "~/Scripts/jquery.dm-uploader.js"));
@@ -25,11 +28,6 @@ namespace PizzaWebsite
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js"));
 
-            // jQuery DM Uploader CSS
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/jQuery.FileUpload/css/jquery.dm-uploader.css"));
-
-            // Site scripts
             bundles.Add(new ScriptBundle("~/bundles/site").Include(
                 "~/Scripts/site.js",
                 "~/Scripts/site-cart.js",
@@ -39,9 +37,23 @@ namespace PizzaWebsite
                 "~/Scripts/site-manage-menu-images.js",
                 "~/Scripts/site-pizza-builder.js"));
 
-            // Site CSS
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+              "~/Content/themes/base/jquery.ui.core.css",
+              "~/Content/themes/base/jquery.ui.resizable.css",
+              "~/Content/themes/base/jquery.ui.selectable.css",
+              "~/Content/themes/base/jquery.ui.accordion.css",
+              "~/Content/themes/base/jquery.ui.autocomplete.css",
+              "~/Content/themes/base/jquery.ui.button.css",
+              "~/Content/themes/base/jquery.ui.dialog.css",
+              "~/Content/themes/base/jquery.ui.slider.css",
+              "~/Content/themes/base/jquery.ui.tabs.css",
+              "~/Content/themes/base/jquery.ui.datepicker.css",
+              "~/Content/themes/base/jquery.ui.progressbar.css",
+              "~/Content/themes/base/jquery.ui.theme.css"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
+                "~/Content/jQuery.FileUpload/css/jquery.dm-uploader.css",
                 "~/Content/site.css",
                 "~/Content/site-pizza-builder.css"));
         }
